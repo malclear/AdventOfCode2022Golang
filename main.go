fec01bf1a9a3bdfc005d066fc3438571fa02c77f
Malcolm Learner <malcolmlearner@gmail.com>
2022-12-06 13:50:32 -0600

    day 6, complete

--- main.go
+++ main.go
@@ -35,22 +35,91 @@ func main() {
 
 func part1(input string) int {
 	parsed := parseInput(input)
-	stacks, linesRead := getArrayOfStacks(parsed)
+	line := parsed[0]
+	fmt.Println("Input data: ")
+	fmt.Println(line)
 
-	linesRead++ //skip blank line
-	for i := linesRead; i < len(parsed); i++ {
-		var count, fromStack, toStack = getMoveData(parsed[i])
-		for j := 0; j < count; j++ {
-			crate, _ := stacks[fromStack-1].Pop()
-			stacks[toStack-1].Push(crate)
+	fmt.Println("Processing -->")
+
+	index := 0
+	var state []int32
+
+	for i, letter := range line {
+		markerReceived, err := markerIsReceived(letter, 4, &state)
+		if err != "" {
+			fmt.Println("!!!!! ERROR !!!!")
+			fmt.Println(err)
+			break
+		}
+		index = i
+		if markerReceived {
+			break
 		}
 	}
-	for i := 0; i < len(stacks); i++ {
-		stack, _ := stacks[i].Pop()
-		fmt.Print(stack)
+	fmt.Println("The marker was found!")
+	for _, l := range state {
+		fmt.Print(string(l))
 	}
 	fmt.Println("")
-	return 0
+	return index + 1
+}
+
+func part2(input string) int {
+	parsed := parseInput(input)
+	line := parsed[0]
+	fmt.Println("Input data: ")
+	fmt.Println(line)
+
+	fmt.Println("Processing -->")
+
+	index := 0
+	var state []int32
+
+	for i, letter := range line {
+		markerReceived, err := markerIsReceived(letter, 14, &state)
+		if err != "" {
+			fmt.Println("!!!!! ERROR !!!!")
+			fmt.Println(err)
+			break
+		}
+		index = i
+		if markerReceived {
+			break
+		}
+	}
+	fmt.Println("The marker was found!")
+	for _, l := range state {
+		fmt.Print(string(l))
+	}
+	fmt.Println("")
+	return index + 1
+}
+
+func markerIsReceived(letter int32, size int, state *[]int32) (bool, string) {
+	if letter == 0 {
+		return false, "A zero value was supplied when checking for a marker."
+	}
+
+	if len(*state) == 0 {
+		*state = append(*state, letter)
+		return false, ""
+	}
+
+	for i := 0; i < len(*state); i++ {
+		if (*state)[i] == letter {
+			// remove i letters from front
+			*state = (*state)[i+1:]
+			// and place letter at back
+			*state = append(*state, letter)
+			return false, ""
+		}
+	}
+	*state = append(*state, letter)
+	if len(*state) < size {
+		return false, ""
+	}
+
+	return true, ""
 }
 
 func getMoveData(line string) (int, int, int) {
@@ -112,30 +181,4 @@ func getContainerStacks(line string) []CrateAddress {
 	}
 
 	return crateAddressList
-}
-
-func part2(input string) int {
-	parsed := parseInput(input)
-	stacks, linesRead := getArrayOfStacks(parsed)
-
-	linesRead++ //skip blank line
-	for i := linesRead; i < len(parsed); i++ {
-		var count, fromStack, toStack = getMoveData(parsed[i])
-		tempStack := Stack{}
-		for j := 0; j < count; j++ {
-			crate, _ := stacks[fromStack-1].Pop()
-			tempStack.Push(crate)
-		}
-		for j := 0; j < count; j++ {
-			crate, _ := tempStack.Pop()
-			stacks[toStack-1].Push(crate)
-		}
-	}
-	for i := 0; i < len(stacks); i++ {
-		stack, _ := stacks[i].Pop()
-		fmt.Print(stack)
-	}
-	fmt.Println("")
-	return 0
-
 }
